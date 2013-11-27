91c44896f6ce98d26334a4f5cd220566509bb3a9
johndewolf <jdewolf06@gmail.com>
2013-11-26 19:14:20 -0500

    Updated Classes

--- grade_test.rb
+++ grade_test.rb
@@ -28,18 +28,59 @@ class Student
   end
 
   def average_score
+    FinalGrade.new(@grades).calc_average_score
+  end
+
+  def letter_grade
+    FinalGrade.new(@grades).calc_letter_grade
+  end
+
+end
+
+class FinalGrade
+  def initialize(grades)
+    @grades = grades
+  end
+
+  def calc_average_score
     @grades.inject(:+) / @grades.count
+  end
+
+  def calc_letter_grade
+    if calc_average_score >= 90
+      'A'
+    elsif calc_average_score>= 80
+      'B'
+    elsif calc_average_score >= 70
+      'C'
+    elsif calc_average_score >= 60
+      'D'
+    else
+      'F'
+    end
   end
 end
 
+# class GradeSummary
+#   def initialize(grades)
+#     @grades = grades
+#   end
+
+#   def average_grades
+#     @grades.count
+#   end
+# end
+
+
 reader = GradeReader.new('students.csv')
 students = reader.import_students
+
 students.each do |student|
   puts "Last Name: #{student.last_name}"
   puts "First Name: #{student.first_name}"
-  puts "Test Scores: " + student.grades
-  # puts student.grades
-  puts "Average Score: #{student.average_score}"
+  puts student.grades
+  puts student.average_score
+  puts student.letter_grade
   puts "____________________"
 end
 
